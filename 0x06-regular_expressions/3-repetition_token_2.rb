1e17e735b6b6a9095fe33f4a610a6cab598f6f94
Cecales <cesar.caleroes@gmail.com>
2022-02-23 21:08:14 -0500

    correction of points 3 and 4

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,4 @@
-#!usr/bin/env ruby
+#!/usr/bin/env ruby
 # This is a Ruby script that accepts one argument and pass it to a regular expression matching methodi
 
 puts ARGV[0].scan(/hbt{1,5}n/).join
