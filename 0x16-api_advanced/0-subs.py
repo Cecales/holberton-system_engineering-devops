5ea2b373488d8e3613ee9006136dcf31f6db0a41
Cecales <cesar.caleroes@gmail.com>
2022-05-20 13:09:58 -0500

    Upload main files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+This is a function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """number of subscribers subreddit"""
+    url = 'https://www.reddit.com/requests/' + subreddit + 'about/json'
+    headers = {
+            'User-Agent': ''
+            }
+    req = requests.get(url, header=headers)
+    data = req.json().get('data').get('subscribers')
+    return data if data is not None
