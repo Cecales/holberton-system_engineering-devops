bec42eba061a037df1f6b5aae26a7fdbc31f9277
Cecales <cesar.caleroes@gmail.com>
2022-05-21 17:23:56 -0500

    Recorrect point 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """number of subscribers subreddit"""
-    url = 'https://www.reddit.com/requests/{}/about/json'.format(subreddit)
-    headers = {'User-Agent': 'Cecales'}
-    req = requests.get(url, headers=headers, allow_redirects=False)
+    req = requests.get('https://www.reddit.com/requests/{}/about/json'
+                       .format(subreddit),
+                       headers = {'User-Agent': 'Cecales'},
+                       allow_redirects=False)
     if req.status_code == 200:
         return req.json().get('data').get('subscribers')
     return 0
